90b36ab3ad67608d56b8e7933c2c1489b412f05f
Luke Hackett <luke-hackett@outlook.com>
2023-01-18 15:21:13 +1100

    init files for task 3

---
+++ 0x01-Webpack/task_3/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+module.exports = {
+    mode: "production",
+    entry: "./js/dashboard_main.js",
+    output: {
+        path: path.resolve(__dirname, "public"),
+        filename: "bundle.js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: [
+                    "file-loader",
+                    {
+                        loader: "image-webpack-loader",
+                        options: {
+                            bypassOnDebug: true,
+                            disable: true,
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+};
