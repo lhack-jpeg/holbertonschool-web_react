765555f3c7dbee12f0167be62bfe1b1e72946667
Luke Hackett <luke-hackett@outlook.com>
2023-01-18 11:58:38 +1100

    add production mode

--- 0x01-Webpack/task_1/webpack.config.js
+++ 0x01-Webpack/task_1/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 module.exports = {
+    mode: "production",
     entry: path.resolve(__dirname, "./js/dashboard_main.js"),
     output: {
         path: path.resolve(__dirname, "public"),
